534fd66099d141a42afafa6d5c1731c16ab927a4
Dmytro (Dima) <64629013+Dima-developer@users.noreply.github.com>
2020-08-30 11:34:19 +0300

    portfolio

--- js/main.js
+++ js/main.js
@@ -1,37 +1,36 @@
 $(document).ready(function () {
     $('.portfolio__btn[filter]').click(function () {
-        if ($(this).attr('filter') == 'all') {
-            if ($(this).attr('val') == 'off') {
-                $('.portfolio__btn[filter]').attr('val', 'off');
-                $(this).attr('val', 'on');
-                $('.portfolio__btn[filter]').removeClass('focused')
-                $(this).addClass('focused');
+        if ($(this).attr('val') == 'off') {
+            $('.portfolio__btn[filter]').attr('val', 'off').removeClass('focused');
+            $(this).attr('val', 'on').addClass('focused');
+            $('.filter > div').hide(300);
+            $('.filter > div[filter=' + $(this).attr('filter') + ']').show(300);
+            if ($(this).attr('filter') == 'all') {
+                $('.portfolio__btn[filter]').attr('val', 'off').removeClass('focused');
+                $(this).attr('val', 'on').addClass('focused');
                 $('.filter > div').show(300);
             }
-        } else
-            if ($(this).attr('val') == 'off') {
-                $('.portfolio__btn[filter]').attr('val', 'off');
-                $(this).attr('val', 'on');
-                $('.portfolio__btn[filter]').removeClass('focused');
-                $(this).addClass('focused');
-                $('.filter > div').hide(300);
-                var filter = $(this).attr('filter');
-                $('.filter > div[filter=' + filter + ']').show(300);
-            }
-    });
-    $('.portfolio__btn[filter="moc"]').click(function () {
-        if ($(this).attr('val') == 'off') {
-            $('.portfolio__btn[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.filter > div').hide(300);
-            $('.filter > div[filter="moc"]').show(300);
         }
-    });
-    $('.portfolio__btn[filter="all"]').click(function () {
-        if ($(this).attr('val') == 'off') {
-            $('.portfolio__btn[filter]').attr('val', 'off');
-            $(this).attr('val', 'on');
-            $('.filter > div').show(300);
-        }
-    });
-});
+    })
+})
+
+                // if ($(this).attr('filter') == 'all') {
+                //     if ($(this).attr('val') == 'off') {
+                //         $('.portfolio__btn[filter]').attr('val', 'off');
+                //         $(this).attr('val', 'on');
+                //         $('.portfolio__btn[filter]').removeClass('focused')
+                //         $(this).addClass('focused');
+                //         $('.filter > div').show(300);
+                //     }
+                // } else
+                //     if ($(this).attr('val') == 'off') {
+                //         $('.portfolio__btn[filter]').attr('val', 'off');
+                //         $(this).attr('val', 'on');
+                //         $('.portfolio__btn[filter]').removeClass('focused')
+                //         $(this).addClass('focused');
+                //         $('.filter > div').hide(300);
+                //         var filter = $(this).attr('filter');
+                //         $('.filter > div[filter=' + filter + ']').show(300);
+//                     }
+//             })
+// });
